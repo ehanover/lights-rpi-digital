82f4f905a5ea7e44bb1040e5b46028190635dfc6
foosbro <ephanover@gmail.com>
2019-01-28 22:13:45 +0000

    working version

--- main.py
+++ main.py
@@ -14,34 +14,37 @@ app.config['ENV'] = 'development'
 
 pixels = neopixel.NeoPixel(board.D18, 30*2)
 
-pixels.fill((1, 1, 1))
+pixels.fill((0, 1, 0))
 #pixels[1] = (255, 0, 0)
 
-current = sp.Popen(['echo', 'test'])
+current = sp.Popen(['echo', ' '])
 
 def start(l):
 	global current
+	print(l)
 	sp.Popen.terminate(current)
 	current = sp.Popen(l)
 
 @app.route('/off')
 def off():
-	global current
-	sp.Popen.terminate(current)
-	pixels.fill((0, 0, 0))
+	start(['python3', 'solid.py', '0', '0', '0'])
+	#global current
+	#sp.Popen.terminate(current)
+	#pixels.fill((0, 0, 0))
 
 	print("lights off.")
 	return "lights off.", 200
 
 @app.route('/solid')
 def solid():
-	global current
-	sp.Popen.terminate(current)
 	r = int(request.args.get("r"))
 	g = int(request.args.get("g"))
 	b = int(request.args.get("b"))
 
-	pixels.fill((r, g, b))
+	start(['python3', 'solid.py', str(r), str(g), str(b)])
+	#global current
+	#sp.Popen.terminate(current)
+	#pixels.fill((r, g, b))
 
 	print("colors set")
 	return "colors set.", 200
@@ -62,9 +65,23 @@ def rainbow():
 
 @app.route('/fade')
 def fade():
+	s = float(request.args.get("s"))
+	t = int(float(request.args.get("t")))
+
+	# Color(0xOORRGGBB)
+	a = request.args.get("a")[10:-1]
+	b = request.args.get("b")[10:-1]
+	c = request.args.get("c")[10:-1]
+	start(['python3', 'fade.py', str(s), str(t), str(a), str(b), str(c)])
 
 	print("started fade")
 	return "started fade.", 200
+
+@app.route('/shutdown')
+def shutdown():
+	off()
+	sp.Popen(['sudo', 'shutdown', '-h', 'now'])
+	return "shutdown.", 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
---
+++ solid.py
@@ -0,0 +1,14 @@
+#import time
+import board
+import neopixel
+import sys
+
+pixel_pin = board.D18
+num_pixels = 60
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=True, pixel_order=ORDER)
+
+c = [int(i) for i in sys.argv[1:]]
+
+pixels.fill(tuple(c))
+
