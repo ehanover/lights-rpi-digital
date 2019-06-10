9ce1c4dbce8d4f72d69467d2ca8c798eabf73ffb
foosbro <ephanover@gmail.com>
2019-06-10 20:06:56 +0100

    fixes to music socket

--- Lights/music_socket.py
+++ Lights/music_socket.py
@@ -13,7 +13,7 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=
 pixels_deque = deque([[0,0,0] for i in range(num_pixels)], maxlen=num_pixels) # pix is the deque that holds rgb values for each light. The oldest value gets bumped out every frame.
 
 
-GLOBAL_SCALE = 0.009 #0.05
+GLOBAL_SCALE = 0.05 #0.009
 FMAX = 2000 # max frequency at dlen=257
 DLEN = 257
 
@@ -43,7 +43,7 @@ pixels.show()
 
 def get_volumes(freqs):
 
-        data = np.abs( np.fft.rfft(freqs[-1]) )
+        data = np.abs( np.fft.rfft(freqs) ) # [-1]
         avg = [0]*len(SECTIONS)
 
         for i in range(len(data)):
@@ -66,7 +66,7 @@ while True:
     # if len(freqs) > 0:
 
     data, addr = socket.recvfrom(1024)
-    volumes = list(data)
+    volumes = get_volumes(list(data))
 
     vals = []
     for i in range(len(volumes)):
